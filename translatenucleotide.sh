3437dc070ecddddae00d5db7884b634cba637697
MartenHoogeveen <martenhoogeveen@gmail.com>
2018-11-02 14:46:50 +0100

    fix

--- translatenucleotide.sh
+++ translatenucleotide.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-transeq -sequence $1 -frame 6 -outseq proteintranslation -table $4
+transeq -sequence $1 -frame 6 -outseq proteintranslation -table $4 2> /dev/null 
 translatenucleotide.py -i proteintranslation -o $2 -s $3
