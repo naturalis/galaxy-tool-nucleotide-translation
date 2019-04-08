0c6096ada5347d4915488b0ca933f6c8663e0d73
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-04-08 14:36:17 +0000

    update

--- translatenucleotide.sh
+++ translatenucleotide.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-#location for production server
-#outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
-#location for the testserver
-#outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
-outlocation=$(mktemp -d /home/galaxy/ExtraRef/XXXXXX)
+outlocation=$(mktemp -d /home/galaxy/galaxy/database/XXXXXX)
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 transeq -sequence $1 -frame 6 -outseq $outlocation"/proteintranslation" -table $4 2> /dev/null 
-translatenucleotide.py -i $outlocation"/proteintranslation" -o $2 -s $3
+python $SCRIPTDIR"/translatenucleotide.py" -i $outlocation"/proteintranslation" -o $2 -s $3
 rm -rf $outlocation
